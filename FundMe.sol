ca5ccac83fac47f63f0aca5cda4b5436d34ecfa7
Rohan Sharma <70716534+ron2111@users.noreply.github.com>
2022-06-09 02:14:19 +0530

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -58,12 +58,7 @@ address public owner; // to setip the owner of contract
  
    }
 
-   function getVersion() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        return priceFeed.version();
-    }
-
-
+ 
     function withdraw() public onlyOwner {
 
          for(uint256 funderIndex =0;funderIndex<funders.length; funderIndex++){
